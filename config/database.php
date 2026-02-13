c81fcf9cc78e689f63dec5760fbaa81f92d6863d
Dario <dariomititeluradu@gmail.com>
2026-02-13 16:44:02 +0100

    modified de line for ssh, again

--- config/database.php
+++ config/database.php
@@ -80,7 +80,6 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
